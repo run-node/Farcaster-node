ccb0661c3d469f8ce6d9defad5ab63b795a23f2d
run-node <160773499+run-node@users.noreply.github.com>
2024-06-21 15:38:19 +0800

    Update Farcaster.sh

--- Farcaster.sh
+++ Farcaster.sh
@@ -11,58 +11,94 @@ fi
 SCRIPT_PATH="$HOME/Farcaster.sh"
 
 function install_node() {
+install_dependencies
 
-    sudo apt-get update
-    
-    # 检查 screen 是否安装
-    if ! command -v screen &> /dev/null
-    then
-        echo "screen 未安装，正在安装..."
-        # 检查操作系统类型
-        if [ -x "$(command -v apt-get)" ]; then
-            sudo apt-get update
-            sudo apt-get install screen -y
-        elif [ -x "$(command -v yum)" ]; then
-            sudo yum install screen -y
-        else
-            echo "无法识别的包管理器，请手动安装 screen"
-            exit 1
-        fi
-    else
-        echo "screen 已安装"
-    fi
-        # 检查 curl 是否安装
-    if ! command -v curl &> /dev/null
-    then
-        echo "curl 未安装，正在安装..."
-        # 检查操作系统类型
-        if [ -x "$(command -v apt-get)" ]; then
-            sudo apt-get update
-            sudo apt-get install curl -y
-        elif [ -x "$(command -v yum)" ]; then
-            sudo yum install curl -y
-        else
-            echo "无法识别的包管理器，请手动安装 curl"
-            exit 1
-        fi
-    else
-        echo "curl 已安装"
-    fi
-    
-    cd $HOME
-    screen -ls | grep Detached | grep hubble | awk -F '[.]' '{print $1}' | xargs -I {} screen -S {} -X quit
-    screen -dmS hubble bash -c 'curl -sSL https://download.thehubble.xyz/bootstrap.sh | bash'
-    # 等待2秒
-    sleep 2
-    echo "节点已启动，请使用 'screen -r hubble' 查看日志。"
-    screen -r hubble
+    mkdir -p ~/hubble
+    local tmp_file=$(mktemp)
+    fetch_file_from_repo "scripts/hubble.sh" "$tmp_file"
+    mv "$tmp_file" ~/hubble/hubble.sh
+    chmod +x ~/hubble/hubble.sh
+
+    cd ~/hubble
+    exec ./hubble.sh "upgrade" < /dev/tty
 }
 
-function restart() {
-    screen -ls | grep Detached | grep hubble | awk -F '[.]' '{print $1}' | xargs -I {} screen -S {} -X quit
-    screen -dmS hubble bash -c 'curl -sL https://hubble.sh | bash'
-    echo "节点已重启，请使用 'screen -r hubble' 查看日志。"
+# 从仓库获取文件
+function fetch_file_from_repo() {
+    local file_path="$1"
+    local local_filename="$2"
+
+    local download_url="https://raw.githubusercontent.com/farcasterxyz/hub-monorepo/@latest/$file_path?t=$(date +%s)"
+    curl -sS -o "$local_filename" "$download_url" || { echo "下载 $download_url 失败。"; exit 1; }
 }
+
+# 安装 jq 工具
+function install_jq() {
+    if command -v jq >/dev/null 2>&1; then
+        echo "jq 已安装。"
+        return 0
+    fi
+
+    echo "安装 jq..."
+
+    if [[ "$(uname)" == "Darwin" ]]; then
+        if command -v brew >/dev/null 2>&1; then
+            brew install jq
+        else
+            echo "未检测到 Homebrew，请先安装 Homebrew。"
+            return 1
+        fi
+    elif [[ -f /etc/lsb-release ]] || [[ -f /etc/debian_version ]]; then
+        sudo apt-get update
+        sudo apt-get install -y jq
+    elif [[ -f /etc/redhat-release ]]; then
+        sudo yum install -y jq
+    elif [[ -f /etc/fedora-release ]]; then
+        sudo dnf install -y jq
+    elif [[ -f /etc/os-release ]] && grep -q "ID=openSUSE" /etc/os-release; then
+        sudo zypper install -y jq
+    elif [[ -f /etc/arch-release ]]; then
+        sudo pacman -S jq
+    else
+        echo "不支持的操作系统，请手动安装 jq。"
+        return 1
+    fi
+
+    echo "jq 安装成功。"
+}
+
+# 安装基本依赖
+function install_dependencies() {
+    sudo apt update
+    sudo apt install pkg-config curl build-essential libssl-dev libclang-dev ufw -y
+
+    # 检查 Git 是否已安装
+    if ! command -v git &> /dev/null; then
+        echo "未检测到 Git，正在安装..."
+        sudo apt install git -y
+    else
+        echo "Git 已安装。"
+    fi
+
+    # 检查 Docker 是否已安装
+    if ! command -v docker &> /dev/null; then
+        echo "未检测到 Docker，正在安装..."
+        sudo apt-get install ca-certificates curl gnupg lsb-release -y
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo chmod a+r /etc/apt/keyrings/docker.gpg
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    else
+        echo "Docker 已安装。"
+    fi
+
+    install_jq
+}
+
 
 function check() {
     cat /root/hubble/.env
@@ -74,11 +110,8 @@ function uninstall() {
 }
 
 function check_service_status() {
-    if screen -list | grep -q "hubble"; then
-        screen -r hubble
-    else
-        echo "没有运行中的 hubble 节点。"
-    fi
+    cd $HOME/hubble
+    docker compose logs -f --tail 20
 }
 
 # 主菜单
@@ -88,17 +121,15 @@ function main_menu() {
     echo "请选择要执行的操作:"
     echo "1. 安装节点"
     echo "2. 查看节点日志"
-    echo "3. 重启节点"
-    echo "4. 查询节点配置信息"
-    echo "5. 卸载节点"
-    read -p "请输入选项（1-5）: " OPTION
+    echo "3. 查询节点配置信息"
+    echo "4. 卸载节点"
+    read -p "请输入选项（1-4）: " OPTION
 
     case $OPTION in
     1) install_node ;;
     2) check_service_status ;;
-    3) restart ;;
-    4) check ;;
-    5) uninstall ;;
+    3) check ;;
+    4) uninstall ;;
     *) 
         echo "无效选项。" 
         read -p "按任意键返回主菜单..."
