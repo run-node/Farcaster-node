e628a2b90913e00fa3d93422179cae84ba0896e0
run-node <160773499+run-node@users.noreply.github.com>
2024-06-30 19:36:42 +0800

    Update Farcaster.sh

--- Farcaster.sh
+++ Farcaster.sh
@@ -10,27 +10,38 @@ fi
 # 脚本保存路径
 SCRIPT_PATH="$HOME/Farcaster.sh"
 
-function install_node() {
-install_dependencies
+# 安装基本依赖
+function install_dependencies() {
+    sudo apt update
+    sudo apt install pkg-config curl build-essential libssl-dev libclang-dev ufw -y
 
-    mkdir -p ~/hubble
-    local tmp_file=$(mktemp)
-    fetch_file_from_repo "scripts/hubble.sh" "$tmp_file"
-    mv "$tmp_file" ~/hubble/hubble.sh
-    chmod +x ~/hubble/hubble.sh
+    # 检查 Git 是否已安装
+    if ! command -v git &> /dev/null; then
+        echo "未检测到 Git，正在安装..."
+        sudo apt install git -y
+    else
+        echo "Git 已安装。"
+    fi
 
-    cd ~/hubble
-    exec ./hubble.sh "upgrade" < /dev/tty
+    # 检查 Docker 是否已安装
+    if ! command -v docker &> /dev/null; then
+        echo "未检测到 Docker，正在安装..."
+        sudo apt-get install ca-certificates curl gnupg lsb-release -y
+        sudo mkdir -p /etc/apt/keyrings
+        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+        echo \
+        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+        sudo chmod a+r /etc/apt/keyrings/docker.gpg
+        sudo apt-get update
+        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
+    else
+        echo "Docker 已安装。"
+    fi
+
+    install_jq
 }
 
-# 从仓库获取文件
-function fetch_file_from_repo() {
-    local file_path="$1"
-    local local_filename="$2"
-
-    local download_url="https://raw.githubusercontent.com/farcasterxyz/hub-monorepo/@latest/$file_path?t=$(date +%s)"
-    curl -sS -o "$local_filename" "$download_url" || { echo "下载 $download_url 失败。"; exit 1; }
-}
 
 # 安装 jq 工具
 function install_jq() {
@@ -67,37 +78,29 @@ function install_jq() {
     echo "jq 安装成功。"
 }
 
-# 安装基本依赖
-function install_dependencies() {
-    sudo apt update
-    sudo apt install pkg-config curl build-essential libssl-dev libclang-dev ufw -y
+# 从仓库获取文件
+function fetch_file_from_repo() {
+    local file_path="$1"
+    local local_filename="$2"
 
-    # 检查 Git 是否已安装
-    if ! command -v git &> /dev/null; then
-        echo "未检测到 Git，正在安装..."
-        sudo apt install git -y
-    else
-        echo "Git 已安装。"
-    fi
-
-    # 检查 Docker 是否已安装
-    if ! command -v docker &> /dev/null; then
-        echo "未检测到 Docker，正在安装..."
-        sudo apt-get install ca-certificates curl gnupg lsb-release -y
-        sudo mkdir -p /etc/apt/keyrings
-        curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
-        echo \
-        "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
-        $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-        sudo chmod a+r /etc/apt/keyrings/docker.gpg
-        sudo apt-get update
-        sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
-    else
-        echo "Docker 已安装。"
-    fi
-
-    install_jq
+    local download_url="https://raw.githubusercontent.com/farcasterxyz/hub-monorepo/@latest/$file_path?t=$(date +%s)"
+    curl -sS -o "$local_filename" "$download_url" || { echo "下载 $download_url 失败。"; exit 1; }
 }
+
+# 安装 hubble 节点
+function install_node() {
+    install_dependencies
+
+    mkdir -p ~/hubble
+    local tmp_file=$(mktemp)
+    fetch_file_from_repo "scripts/hubble.sh" "$tmp_file"
+    mv "$tmp_file" ~/hubble/hubble.sh
+    chmod +x ~/hubble/hubble.sh
+
+    cd ~/hubble
+    exec ./hubble.sh "upgrade" < /dev/tty
+}
+
 
 
 function check() {
@@ -105,13 +108,16 @@ function check() {
 }
 
 function uninstall() {
+    cd $HOME/hubble
+    docker compose down -v
+    cd $HOME
     rm -rf $HOME/hubble
     echo "节点已卸载。"
 }
 
 function check_service_status() {
-    cd $HOME/hubble
-    docker compose logs -f --tail 20
+    cd $HOME
+    /root/hubble/hubble.sh logs
 }
 
 # 主菜单
@@ -130,8 +136,8 @@ function main_menu() {
     2) check_service_status ;;
     3) check ;;
     4) uninstall ;;
-    *) 
-        echo "无效选项。" 
+    *)
+        echo "无效选项。"
         read -p "按任意键返回主菜单..."
         main_menu
         ;;
